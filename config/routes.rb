e14455c49f2b4a8735648b20ffdbd45cc2907e42
Paloma Rod <placeholder.prod@outlook.com>
2023-10-09 00:46:05 -0600

    so much crap

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   # get "about-us", to: "about#index", as: :about
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   # get "", to: "main#index"
   root to: "main#index"
 end
